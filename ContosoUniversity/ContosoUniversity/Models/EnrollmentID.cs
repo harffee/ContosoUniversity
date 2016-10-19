2d3cc1064187a6f6483982a2473ac018b874a864
v-jzingh <v-jzingh@VISUAL-S>
2016-10-19 15:05:23 +0800

    Create an EF data model

---
+++ ContosoUniversity/ContosoUniversity/Models/EnrollmentID.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        //The question mark after the Grade type declaration indicates that the Grade property is nullable
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
